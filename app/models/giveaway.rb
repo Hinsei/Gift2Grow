2bf232047b7152877211b493f0105e9142ca1dba
Aiman <imbacakes@gmail.com>
2017-01-12 12:16:17 +0800

    added validations, uploaders and associations

--- app/models/giveaway.rb
+++ app/models/giveaway.rb
@@ -1,2 +1,21 @@
 class Giveaway < ApplicationRecord
+	require 'carrierwave/orm/activerecord'
+
+	#Image Uploading
+	mount_uploader :prize_image, PrizeImageUploader
+	mount_uploader :terms_and_condition, TermsAndConditionUploader
+
+	#Validations
+	validates :title, presence: true
+	validates :prize_name, presence: true
+	validates	:prize_image, presence: true
+	validates	:terms_and_condition, presence: true
+	validates	:winner_message, presence: true
+	validates	:consolation_message, presence: true
+	validates	:num_winner, presence: true
+	validates :num_backup, presence: true
+
+	#Associations
+	belongs_to :company
+
 end
