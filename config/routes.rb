1b2b28b7d27115d7382d8cfafd5f127943e43246
user <likhimsiow@yahoo.com>
2017-01-12 12:52:44 +0800

    Created session controller methods and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :sessions
+resources :sessions, only: [:new, :create, :destroy]
 
+resources :companies, only: [:new, :edit, :show, :update, :destroy] do
+	resources :giveaway
+end
+
+root 'welcome#index'
 end
