76cd7768a698d8bafc39feee854477edfe77b573
Aiman <imbacakes@gmail.com>
2017-01-13 12:09:51 +0800

    added helper to check winners

--- app/helpers/giveaways_helper.rb
+++ app/helpers/giveaways_helper.rb
@@ -7,4 +7,18 @@ module GiveawaysHelper
 			true
 		end
 	end
+
+	def winner?
+		counter = 0
+		@company.giveaways.each do |giveaway|
+			unless giveaway.winner.winners.nil?
+				counter += 1
+			end
+		end
+		if counter == 0
+			false
+		else
+			true
+		end
+	end
 end
