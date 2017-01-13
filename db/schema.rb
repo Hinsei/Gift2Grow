2de598b7c097473d21761132c6bc1efa42910a4a
Aiman <imbacakes@gmail.com>
2017-01-13 11:55:22 +0800

    added has one relationship of giveaway with winner

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170112034243) do
+ActiveRecord::Schema.define(version: 20170113034520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
