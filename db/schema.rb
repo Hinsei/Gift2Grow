7c93e35adab7860ef8cece3281c30ee5f2c5635e
user <likhimsiow@yahoo.com>
2017-01-13 18:40:01 +0800

    added some design

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 20170113042023) do
     t.integer  "num_backup"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+    t.datetime "start_date"
+    t.datetime "end_date"
     t.integer  "status"
     t.index ["company_id"], name: "index_giveaways_on_company_id", using: :btree
   end
